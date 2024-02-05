d9296e86a0aeecee57ffffbd1c3d80d39bdb7f57
mehdimediouni <38542330+mehdimediouni@users.noreply.github.com>
2024-02-05 18:02:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
-streamlit.title('we are trying a new recepe v2')
 streamlit.header('Breakfast Menu')
-streamlit.text('café & croissants')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
